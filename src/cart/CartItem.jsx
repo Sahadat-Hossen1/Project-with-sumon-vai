2e4376d16eec62847b5fd6a5065bab3c7cf260df
Sahadat-Hossen1 <sahadat.hossen1435@gmail.com>
2026-01-18 10:16:26 +0600

    make modal and cart dynamic

--- src/cart/CartItem.jsx
+++ src/cart/CartItem.jsx
@@ -1,7 +1,35 @@
 import React from 'react'
 
-export default function CartItem() {
+export default function CartItem({cart}) {
+  const {id,image,title,price,quantity}=cart
+  
   return (
-    <div>CartItem</div>
+    <div>
+      <div className="flex items-center gap-4 p-4 bg-white rounded-2xl shadow">
+        <img
+          src={image}
+          alt={title}
+          className="w-20 h-20 rounded-xl object-cover"
+        />
+
+        <div className="flex-1">
+          <h3 className="text-lg font-semibold">{title}</h3>
+          <p className="text-gray-500">৳ {price}</p>
+
+          <div className="flex items-center gap-3 mt-2">
+            <button className="w-8 h-8 bg-gray-200 rounded-full">-</button>
+            <span className="font-medium">{quantity}</span>
+            <button className="w-8 h-8 bg-gray-200 rounded-full">+</button>
+          </div>
+        </div>
+
+        <div className="text-right">
+          <p className="font-semibold">{price*quantity} </p>
+          <button className="text-red-500 text-sm mt-2">
+            Remove
+          </button>
+        </div>
+      </div>
+    </div>
   )
 }
--- src/cart/CartList.jsx
+++ src/cart/CartList.jsx
@@ -1,64 +1,70 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useReducer } from "react";
+import CartReducer, { initialState } from "../reducers/Cart";
+import { getProducts } from "../api_service/Product";
+import Actions from "../Actions";
+import EndPoints from "../api_service/EndPoints";
+import ProductSkeletonList from "../component/ProductSkeleton";
+import ErrorCart from "../component/ErrorCart";
+import NoProductFound from "../component/NoProductFound";
+import CartItem from "./CartItem";
 
 export default function CartList() {
-  
-  return (
-    <div className="max-w-3xl mx-auto mt-10 space-y-4">
-      
-      {/* Cart Item */}
-      <div className="flex items-center gap-4 p-4 bg-white rounded-2xl shadow">
-        <img
-          src="https://via.placeholder.com/150"
-          alt="product"
-          className="w-20 h-20 rounded-xl object-cover"
-        />
+  const [state, dispatch] = useReducer(CartReducer, initialState);
+  const { loading, data, error } = state || {};
+  console.log(data);
 
-        <div className="flex-1">
-          <h3 className="text-lg font-semibold">Wireless Headphone</h3>
-          <p className="text-gray-500">৳ 1200</p>
+  useEffect(() => {
+    (async () => {
+      dispatch({
+        type: Actions.Cart.DATA_FETCHING,
+      });
+      try {
+        const data = await getProducts(EndPoints.CARTS);
+        // console.log(data);
 
-          <div className="flex items-center gap-3 mt-2">
-            <button className="w-8 h-8 bg-gray-200 rounded-full">-</button>
-            <span className="font-medium">1</span>
-            <button className="w-8 h-8 bg-gray-200 rounded-full">+</button>
-          </div>
-        </div>
+        dispatch({
+          type: Actions.Cart.DATA_FETCHED,
+          payload: {
+            data: data,
+          },
+        });
+      } catch (error) {
+        dispatch({
+          type: Actions.Cart.DATA_FETCH_ERROR,
+          payload: {
+            error: error,
+          },
+        });
+      }
+    })();
+    console.log(state);
+  }, []);
 
-        <div className="text-right">
-          <p className="font-semibold">৳ 1200</p>
-          <button className="text-red-500 text-sm mt-2">
-            Remove
-          </button>
-        </div>
+  let content = null;
+  if (loading) {
+    content = <h1 className="text-3xl text-center text-red-700">Loading ......</h1>;
+  }
+  if (!loading && error) {
+    content = <p> something went wrong</p>;
+  }
+  if (!loading && !error && data.length === 0) {
+    content = <p>Emty cart</p>
+  }
+  if (!loading && !error && data.length > 0) {
+    content = (
+      <div className="max-w-3xl mx-auto mt-10 space-y-4">
+        {data.map((cart) => (
+          <CartItem cart={cart} key={cart.id}/>
+        ))}
       </div>
-
-      {/* Cart Item */}
-      <div className="flex items-center gap-4 p-4 bg-white rounded-2xl shadow">
-        <img
-          src="https://via.placeholder.com/150"
-          alt="product"
-          className="w-20 h-20 rounded-xl object-cover"
-        />
-
-        <div className="flex-1">
-          <h3 className="text-lg font-semibold">Smart Watch</h3>
-          <p className="text-gray-500">৳ 2500</p>
-
-          <div className="flex items-center gap-3 mt-2">
-            <button className="w-8 h-8 bg-gray-200 rounded-full">-</button>
-            <span className="font-medium">2</span>
-            <button className="w-8 h-8 bg-gray-200 rounded-full">+</button>
-          </div>
-        </div>
-
-        <div className="text-right">
-          <p className="font-semibold">৳ 5000</p>
-          <button className="text-red-500 text-sm mt-2">
-            Remove
-          </button>
-        </div>
-      </div>
-
-    </div>
-  );
+    );
+  }
+  // return (
+  //   <div className="max-w-3xl mx-auto mt-10 space-y-4">
+  //     {data.map((cart) => (
+  //       <CartItem cart={cart} key={cart.id} />
+  //     ))}
+  //   </div>
+  // );
+  return content
 }
