71ec2e4d3ecded37d02c83187e36e780bb73456c
Sahadat-Hossen1 <sahadat.hossen1435@gmail.com>
2026-01-18 07:14:39 +0600

    fixed modal possition issue

--- src/component/Modal.jsx
+++ src/component/Modal.jsx
@@ -6,10 +6,11 @@ export default function Modal({ children }) {
     <>
       <div className="w-full h-[100vh] bg-[#191818c6] z-10  fixed top-0 left-0  ">
         {/* {children} */}
-      </div>
-      <div className="w-1/2 h-80 bg-white z-50 fixed top-1/2 left-1/2 -translate-x-1/2 translate-y-1/2 ">
+        <div className="w-1/2 h-80 bg-white z-50 overflow-y-auto fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">
         {children}
       </div>
+      </div>
+      
     </>
   );
 }
